b4269688daa5d32e0aa920e6947fdbd0410c44d5
YuniPG <yunipgx@protonmail.com>
2025-07-26 18:45:13 +0000

    Changue Languages
    Changes on messages.

--- resources/lang/en/validation.php
+++ resources/lang/en/validation.php
@@ -53,6 +53,7 @@ return [
         'numeric' => 'The :attribute must be greater than :value.',
         'file' => 'The :attribute must be greater than :value kilobytes.',
         'string' => 'The :attribute must be greater than :value characters.',
+        'string' => 'The :attribute must be greater than :value characters.',
         'array' => 'The :attribute must have more than :value items.',
     ],
     'gte' => [
